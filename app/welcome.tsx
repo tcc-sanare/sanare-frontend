c80a1a2409278574d59bf9107aec07712f916d61
Joice Soares de Almeida <joicesoaresalmeida@gmail.com>
2025-05-06 18:32:11 -0300

    welcome feita, iniciar login/cadastro - 06/05

--- app/welcome.tsx
+++ app/welcome.tsx
@@ -1,13 +1,14 @@
+import MaskedView from '@react-native-masked-view/masked-view';
 import * as Font from 'expo-font';
 import { LinearGradient } from 'expo-linear-gradient';
+import { Stack, useRouter } from "expo-router";
 import { useEffect, useState } from 'react';
 import { ActivityIndicator, Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import Colors from '../constants/Colors';
 import Fonts from '../constants/Fonts';
 
-
 export default function Welcome() {
-
+    const router = useRouter();
     const [fontsLoaded, setFontsLoaded] = useState(false);
 
     useEffect(() => {
@@ -28,52 +29,68 @@ export default function Welcome() {
     }
 
     return (
-        <View style={styles.container}>
+        <>
+            <Stack.Screen options={{ headerShown: false }} />
 
-            <View style={styles.logoContainer}>
+            <View style={styles.container}>
                 <Image
-                    source={require('../assets/images/logo.png')}
-                    style={styles.logo}
+                    source={require('../assets/images/bgWelcome.png')}
+                    style={styles.logoFooter}
                 />
-                <Image
-                    source={require('../assets/images/sanare.png')}
-                    style={styles.sanare}
-                />
+
+                <View style={styles.logoContainer}>
+                    <Image
+                        source={require('../assets/images/logo.png')}
+                        style={styles.logo}
+                    />
+                    <Image
+                        source={require('../assets/images/sanare.png')}
+                        style={styles.sanare}
+                    />
+                </View>
+
+                <View style={styles.viewText}>
+                    <Text style={styles.text}>Esteja</Text>
+
+                    <MaskedView
+                        maskElement={<Text style={[styles.text, styles.gradientText]}>conectado</Text>}>
+                        <LinearGradient
+                            colors={['#005EB7', '#64AEEA']}
+                            start={{ x: 0, y: 0 }}
+                            end={{ x: 1, y: 0 }}>
+                            <Text style={[styles.text, styles.gradientText, { opacity: 0 }]}>  conectado</Text>
+                        </LinearGradient>
+                    </MaskedView>
+
+                    <Text style={styles.text}> com a sua saúde!</Text>
+                </View>
+
+                <View style={styles.containerBtns}>
+
+                    <TouchableOpacity style={styles.btn} onPress={() => router.push('./login/login')}>
+                        <LinearGradient
+                            colors={['#005EB7', '#4697E4']}
+                            start={{ x: 0, y: 0 }}
+                            end={{ x: 1, y: 0.5 }}
+                            style={styles.btnGradient}
+                        >
+                            <Text style={styles.btnText}>Login</Text>
+                        </LinearGradient>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.btn} onPress={() => router.push('./cadastro/cadastro')}>
+                        <LinearGradient
+                            colors={['#005EB7', '#4697E4']}
+                            start={{ x: 0, y: 0 }}
+                            end={{ x: 1, y: 0.5 }}
+                            style={styles.btnGradient}
+                        >
+                            <Text style={styles.btnText}>Cadastro</Text>
+                        </LinearGradient>
+                    </TouchableOpacity>
+                </View>
             </View>
-
-            <Text style={styles.text}>Esteja conectado com a sua saúde!</Text>
-
-            <View style={styles.containerBtns}>
-                <TouchableOpacity style={styles.btn}>
-
-                    <LinearGradient
-                        colors={['#005EB7', '#4697E4']}
-                        start={{ x: 0, y: 0 }}
-                        end={{ x: 1, y: 0.5 }}
-                        style={styles.btnGradient}
-                    >
-                        <Text style={styles.btnText}>Login</Text>
-                    </LinearGradient>
-
-                </TouchableOpacity>
-
-                <TouchableOpacity style={styles.btn}>
-                    <LinearGradient
-                        colors={['#005EB7', '#4697E4']}
-                        start={{ x: 0, y: 0 }}
-                        end={{ x: 1, y: 0.5 }}
-                        style={styles.btnGradient}
-                    >
-                        <Text style={styles.btnText}>Cadastro</Text>
-                    </LinearGradient>
-                </TouchableOpacity>
-            </View>
-
-            <Image
-                source={require('../assets/images/bgWelcome.png')}
-                style={styles.logoFooter}
-            />
-        </View>
+        </>
     )
 }
 
@@ -88,24 +105,30 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
         flexDirection: 'row',
-        gap: 5,
+        gap: 10,
         marginBottom: 50
     },
     logo: {
         resizeMode: 'contain',
-        height: 40
     },
     sanare: {
         resizeMode: 'contain',
-        height: 30,
+        width: 120
+    },
+    viewText: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        marginBottom: 110
     },
     text: {
         fontFamily: 'Poppins-Regular',
         textAlign: 'center',
         color: Colors.light.bluePrimary,
         fontSize: 30,
-        width: '80%',
-        marginBottom: 110
+    },
+    gradientText: {
+        fontFamily: 'Poppins-Bold',
     },
     containerBtns: {
         gap: 30,
@@ -120,7 +143,6 @@ const styles = StyleSheet.create({
         color: Colors.light.textWhite,
         fontFamily: 'Poppins-Medium',
         fontSize: 25,
-        // zIndex: 1,
     },
     btnGradient: {
         flex: 1,
@@ -132,7 +154,6 @@ const styles = StyleSheet.create({
         position: 'absolute',
         bottom: 0,
         top: '20%',
-        // zIndex: -1,
         resizeMode: 'contain',
         left: 0,
         right: 0,
